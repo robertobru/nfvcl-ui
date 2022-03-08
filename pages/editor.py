cafffe465649a1a07b4faa05725bbfdeaef680a3
robertobru <roberto.bruschi@cnit.it>
2022-03-08 10:50:08 +0100

    rest editor

--- pages/editor.py
+++ pages/editor.py
@@ -1,5 +1,5 @@
 import dash_ace
-
+from dash_extensions.enrich import Output, Input, State
 from apps.utils import *
 
 
@@ -10,24 +10,68 @@ class Editor(WebPage):
 
     def get_page_layout(self, value=[]):
         return dbc.Row([
-            dbc.Col(
-                [
+            getTile(
+                dbc.Col([
+                    dbc.Row([
+                        dcc.Dropdown(
+                            options=[
+                                {"label": "POST", "value": "POST"},
+                                {"label": "GET", "value": "GET"},
+                                {"label": "PUT", "value": "PUT"},
+                                {"label": "DELETE", "value": "DELETE"}
+                            ],
+                            id='rest_http_method',
+                            style={'width': '30%', 'padding-left': '3px', 'padding-right': '5px'}
+                        ),
+                        dcc.Input(id='rest_http_path', style={'width': '70%'}, placeholder='URL path')
+                    ], style={'padding': '3px'}),
+                    html.Div(id='rest_editor_message_bar'),
                     dash_ace.DashAceEditor(
                         id='blue_editor',
-                        value='def test(a: int) -> str : \n'
-                              '    return f"value is {a}"',
+                        value='{} \n',
                         theme='github',
                         mode='json',
                         tabSize=2,
                         # enableBasicAutocompletion=True,
                         # enableLiveAutocompletion=True,
                         # autocompleter='/autocompleter?prefix=',
-                        placeholder='Python code ...'
-                    )
-                ],
-                width={"size": 12}),
-
+                        placeholder='JSON object ...',
+                        width='100%',
+                        height='500px',
+                        style={'margin-top': '5px', 'margin-bottom': '5px'}
+                    ),
+                    dbc.Button([html.I(className='fas fa-arrow-circle-right'), "  Submit"], id='rest_submit_button',
+                               color="primary", style={'margin-top': '5px', 'margin-bottom': '5px'})
+                ]),
+                width=6,
+                style={'height': "700px", 'margin-left': '10px'},
+                color='light',
+                title="REST Request"
+            ),
+            getTile(
+                [],
+                width=6,
+                style={'height': "700px"},
+                color='light',
+                title="REST Response"
+            )
         ])
 
     def get_callbacks(self, app):
-        pass
+        @app.callback(Output('rest_editor_message_bar', 'children'),
+                      Input('rest_submit_button', 'n_clicks'),
+                      State('blue_editor', 'value'))
+        def rest_submit_button(click, value):
+            if click is None:
+                return
+
+            print("submit button clicked")
+            print(value)
+            try:
+                body = json.loads(value)
+                print(body)
+                return "Valid JSON"
+            except Exception as e:
+                return "Not a valid JSON"
+
+
